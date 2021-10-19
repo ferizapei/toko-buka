d003ce1512328025088db500957c3a0dc25d5c57
ferizapei <ferizavebrian@gmail.com>
2021-10-19 22:38:34 +0700

    disini aja

---
+++ app/src/main/java/com/example/disiniaja/CariActivity.kt
@@ -0,0 +1,11 @@
+package com.example.disiniaja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CariActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cari)
+    }
+}
